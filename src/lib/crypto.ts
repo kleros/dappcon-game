eb2659eecb828f78219bfb668f2c51693a914f82
alcercu <333aleix333@gmail.com>
2024-05-07 16:33:22 +0200

    refactor: change utf8 to utf-8 to be consistent

--- src/lib/crypto.ts
+++ src/lib/crypto.ts
@@ -17,11 +17,11 @@ export const encrypt = (data: string): string => {
 };
 
 export const decrypt = (encryptedData: string) => {
-  const components = encryptedData.split(":");
-  const iv = Buffer.from(components[0], "hex");
+  const [raw_iv, cipherText] = encryptedData.split(":");
+  const iv = Buffer.from(raw_iv, "hex");
   const secretKey = Buffer.from(RAW_SECRET_KEY, "hex");
   const decipher = crypto.createDecipheriv("aes-256-cbc", secretKey, iv);
-  let decryptedData = decipher.update(components[1], "hex", "utf8");
-  decryptedData += decipher.final("utf8");
+  let decryptedData = decipher.update(cipherText, "hex", "utf-8");
+  decryptedData += decipher.final("utf-8");
   return decryptedData;
 };
