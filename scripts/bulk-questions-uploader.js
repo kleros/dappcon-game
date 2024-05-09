df1ca299dc87d6d627b7950e41859af097be6831
Nikhil <32412967+nikhilverma360@users.noreply.github.com>
2024-05-09 17:38:19 +0530

    fix: uploading rows in batches

--- scripts/bulk-questions-uploader.js
+++ scripts/bulk-questions-uploader.js
@@ -8,37 +8,53 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
-    console.error("Please provide a SUPABASE_URL & SUPABASE_KEY in the .env file.");
-    process.exit(1);
-  }
+  console.error(
+    "Please provide a SUPABASE_URL & SUPABASE_KEY in the .env file."
+  );
+  process.exit(1);
+}
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-async function uploadData() {
+async function batchUpload() {
   try {
-    const results = [];
+    const batchSize = 100; // Adjust batch size based on system or database's capabilities
+    let chunk = [];
     fs.createReadStream(path.resolve(__dirname, "questions.csv"))
       .pipe(csv())
-      .on("data", (data) => results.push(data))
-      .on("end", async () => {
-        const records = results.map((row) => ({
-          question: row.question,
-          answers: [row.option_1, row.option_2, row.option_3, row.option_4],
-        }));
+      .on("data", (data) => {
+        chunk.push({
+          question: data.question,
+          answers: [data.option_1, data.option_2, data.option_3, data.option_4],
+        });
 
-        // Insert records into Supabase
-        const { data, error } = await supabase
-          .from("questions")
-          .insert(records);
-        if (error) {
-          console.error("Error uploading data:", error.message);
-          return;
+        if (chunk.length === batchSize) {
+          uploadData(chunk);
+          chunk = [];
         }
-        console.log("Data uploaded successfully");
+      })
+      .on("end", async () => {
+        if (chunk.length > 0) {
+          uploadData(chunk); // Upload any remaining rows
+        }
+        console.log("🔥 All Batch uploaded successfully 🔥");
       });
   } catch (error) {
     console.error("Error:", error.message);
   }
 }
 
-uploadData();
+async function uploadData(records) {
+  try {
+    const { error } = await supabase.from("questions").insert(records);
+    if (error) {
+      console.error("Error uploading data:", error.message);
+      return;
+    }
+    console.log("Batch uploaded ✅");
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+batchUpload();
