a59b90180be390e2e4369160a28813325ca763ad
Aleix <80388279+alcercu@users.noreply.github.com>
2024-05-13 12:15:41 +0200

    Revert "fix: only redirect to home from middleware if the user exists"

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,14 @@
 import { NextResponse, type NextRequest } from "next/server";
-import { getUserId, TOKEN_COOKIE } from "@/lib/auth";
+import { TOKEN_COOKIE } from "@/lib/auth";
 
 
-export const middleware = async (request: NextRequest) => {
+export const middleware = (request: NextRequest) => {
   const path = request.nextUrl.pathname;
   const isPublicPath = path === "/auth";
   const token = request.cookies.get(TOKEN_COOKIE);
 
   if (isPublicPath && token) {
-    const userId = await getUserId(token.value);
-    if (userId) {
-      return NextResponse.redirect(new URL("/", request.nextUrl));
-    }
+    return NextResponse.redirect(new URL("/", request.nextUrl));
   }
 
   if (!isPublicPath && !token) {
