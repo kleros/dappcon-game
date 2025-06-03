9f5f93764cafdcd02946d83c19474757da8eb5b3
alcercu <333aleix333@gmail.com>
2025-06-03 16:39:26 +0200

    chore: update game dates

--- src/lib/game.config.ts
+++ src/lib/game.config.ts
@@ -1,7 +1,7 @@
 "use strict";
-export const GAME_END_TIMESTAMP = 1716508800; // Timestamp in seconds when the game ends (Fri May 24 2024 00:00:00 GMT+0000)
-const GAME_CONCLUDES_TIMESTAMP = 1717977600; // Timestamp in seconds when the game concludes (Mon Jun 10 2024 00:00:00 GMT+0000)
-export const TOKEN_DISTRIBUTION_TIMESTAMP = 1722211200; // Timestamp in seconds for token distribution (Mon Jul 29 2024 00:00:00 GMT+0000)
+export const GAME_END_TIMESTAMP = 1749559005; // Timestamp in seconds when the game ends (Fri May 24 2024 00:00:00 GMT+0000)
+const GAME_CONCLUDES_TIMESTAMP = 1749645405; // Timestamp in seconds when the game concludes (Mon Jun 10 2024 00:00:00 GMT+0000)
+export const TOKEN_DISTRIBUTION_TIMESTAMP = 1749731805; // Timestamp in seconds for token distribution (Mon Jul 29 2024 00:00:00 GMT+0000)
 
 export const QR_CODE_EXPIRY =2 * 60 * 1000; // (2 minutes) Expiry time in seconds for QR code
 export const QUESTION_TIMEOUT_WINDOW = 3 * 60 * 1000; // (3 minutes) Player cannot answer after question gets timeout
