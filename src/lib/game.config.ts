339594c77c83e3322ff0171533c54559f94fd445
alcercu <333aleix333@gmail.com>
2025-06-13 18:13:31 +0200

    chore: update game timestamps

--- src/lib/game.config.ts
+++ src/lib/game.config.ts
@@ -1,10 +1,7 @@
 "use strict";
-// export const GAME_END_TIMESTAMP = 1750291199; // Timestamp in seconds when the game ends
-// const GAME_CONCLUDES_TIMESTAMP = 1749998988; // Timestamp in seconds when the game concludes
-// export const TOKEN_DISTRIBUTION_TIMESTAMP = 1759998988; // Timestamp in seconds for token distribution
-export const GAME_END_TIMESTAMP = 1749823200; // Timestamp in seconds when the game ends
-const GAME_CONCLUDES_TIMESTAMP = 1749824100; // Timestamp in seconds when the game concludes
-export const TOKEN_DISTRIBUTION_TIMESTAMP = 1749827700; // Timestamp in seconds for token distribution
+export const GAME_END_TIMESTAMP = 1750283999; // Timestamp in seconds when the game ends
+const GAME_CONCLUDES_TIMESTAMP = 1750975199; // Timestamp in seconds when the game concludes
+export const TOKEN_DISTRIBUTION_TIMESTAMP = 1751320799; // Timestamp in seconds for token distribution
 
 export const QR_CODE_EXPIRY = 2 * 60 * 1000; // (2 minutes) Expiry time in seconds for QR code
 export const QUESTION_TIMEOUT_WINDOW = 3 * 60 * 1000; // (3 minutes) Player cannot answer after question gets timeout
